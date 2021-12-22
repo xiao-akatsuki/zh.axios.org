599ee9a050e6b1abd5cb1e696e90c6653dffc1e3
xiaoxunyao <huchengyea@163.com>
2021-12-22 19:05:25 +0800

    init config js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    base: '/zh.axios.org/',
+    lang: 'zh-CN',
+    title: 'axios',
+    description: '一个让java更容易发送HTTPS',
+    themeConfig: {
+        navbar: [
+          {
+            text: '配置',
+            link: '/guide/',
+          },
+          {
+            text: '提出问题',
+            link: 'https://github.com/xiao-akatsuki/axios/issues'
+          },
+          {
+            text: '参与项目',
+            link: 'https://github.com/xiao-akatsuki/axios/pulls'
+          },
+          {
+            text: '版本',
+            children: [
+              {
+                text: '1.0.0',
+                link: '/',
+              },
+            ]
+          }
+        ],  
+        sidebar: [
+          '/guide/README.md',
+          '/guide/MinimalExample.md',
+          '/guide/POSTRequests.md'
+        ],
+        sidebarDepth: 2,
+        logo: '/logo/logo.png',
+        repoLabel:'axios',
+        repo: 'xiao-akatsuki/axios',
+        editLink: false,
+        darkMode: false,
+    },
+}
